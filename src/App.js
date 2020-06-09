d2ed30a8a504a6012b6a97b7518dba5f2ad5469b
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-09 03:52:56 -0400

    Completed step 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
@@ -10,7 +10,7 @@ function App() {
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
+          <div className="home"clear>
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
